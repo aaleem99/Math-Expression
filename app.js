79f2cc37f4df95bf2c01f28881b8e59063f2c5c4
aaleem99 <135497127+aaleem99@users.noreply.github.com>
2024-10-06 13:36:47 +0500

    Add files via upload
    Math Expression

---
+++ app.js
@@ -0,0 +1,47 @@
+var num1=5;
+var num2=3;
+document.write(
+    'sum of 3 and 5 is ', num1+num2, '<br>');
+document.write(
+    'substraction of 3 and 5 is ', num1-num2, '<br>');
+document.write(
+    'Multiplaction of 3 and 5 is ', num1*num2, '<br>');
+document.write(
+    'Division of 3 and 5 is ', num1/num2, '<br>');
+document.write(
+    'Modulus of 3 and 5 is ', num1%num2, '<br>');
+
+var num3=4
+document.write(
+    'Value after Decleration is ', num3, '<br>');
+document.write(
+    'Increment Value ', ++num3, '<br>');
+    num3=num3+7
+    document.write(
+        'Value after Decleration is ', num3, '<br>');
+    
+    document.write(
+        'Value after Decleration is ', --num3, '<br>');
+    document.write(
+        'The remainder is : ', num3%3, '<br>');
+var ticket=600
+document.write(
+    'Total cost to buy ticket is ',ticket*5,'PKR','<br>'
+);
+var table=6
+document.write(
+    'Table of 6', '<br>',
+    '6 x 1= ',table*1,'<br>',
+    '6 x 2= ',table*2,'<br>',
+    '6 x 3= ',table*3,'<br>',
+    '6 x 4= ',table*4,'<br>',
+    '6 x 5= ',table*5,'<br>',
+    '6 x 6= ',table*6,'<br>',
+    '6 x 7= ',table*7,'<br>',
+    '6 x 8= ',table*8,'<br>',
+    '6 x 9= ',table*9,'<br>',
+    '6 x 10= ',table*10,'<br>');
+
+
+
+    
